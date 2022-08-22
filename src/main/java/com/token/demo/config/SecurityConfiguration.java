cd4f741cac07308ac7dd572dc38a536c414129d0
ngactive99 <ngactive99@gmail.com>
2022-08-22 09:36:50 +0530

    generated JWT token

--- src/main/java/com/token/demo/config/SecurityConfiguration.java
+++ src/main/java/com/token/demo/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import com.token.demo.service.UserService;
@@ -27,6 +28,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		// TODO Auto-generated method stub
 		return super.authenticationManagerBean();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated();
 	}
 
 }
--- src/main/java/com/token/demo/controller/HomeController.java
+++ src/main/java/com/token/demo/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,8 +39,12 @@ public class HomeController {
 		} catch (BadCredentialsException e) {
 			throw new Exception("INVALID_CRENDENTIALS", e);
 		}
-		
-		
+
+		final UserDetails userDetails = userService.loadUserByUsername(jwtRequest.getUsername());
+		final String token = jwtUtility.generateToken(userDetails);
+
+		return new JwtResponse(token);
+
 	}
 
 }
